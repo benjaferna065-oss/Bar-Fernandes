3ba80d2d1fe651596ce14b40d29367609b3a293a
benjaferna065-oss <benjaferna065@gmail.com>
2026-01-25 20:37:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ async function perguntarIA() {
     // Verifique se a sua URL está exatamente assim (usando v1 e o modelo flash)
 // Mude para v1beta e adicione -latest no final do modelo
     // A URL precisa ser exatamente assim para o modelo Flash na v1beta
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
     const dadosParaEnviar = {
         contents: [{
             parts: [{
@@ -97,7 +97,8 @@ async function perguntarIA() {
 
 }
 
-console.log("GARCOM ACORDANDO!!!");
+console.log("DESSA VEZ O GARÇOM VEM!!!");
+
 
 
 
